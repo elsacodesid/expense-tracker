f3c5993527e4000f37f58f646c14101e77f8f241
elsacodesid <elsacodesid@gmail.com>
2024-05-20 11:20:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Container, Flex, Box } from '@chakra-ui/react'
+
+function App() {
+
+
+  return (
+  <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
+    <Flex height={"full"}>
+<Box>
+  
+</Box>
+    </Flex>
+  </Container>
+  )
+}
+
+export default App
