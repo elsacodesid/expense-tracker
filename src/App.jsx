ede60b0aca89f15c6f02d732c5d6a91d09701d00
elsacodesid <elsacodesid@gmail.com>
2024-05-20 13:38:06 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Container, Flex, Box } from '@chakra-ui/react'
+import Main from './components/main'
 
 function App() {
 
@@ -10,8 +8,8 @@ function App() {
   return (
   <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
     <Flex height={"full"}>
-<Box>
-  
+<Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+  <Main />
 </Box>
     </Flex>
   </Container>
---
+++ src/components/expense-view/index.jsx
@@ -0,0 +1,6 @@
+
+
+export default function ExpenseView() {
+    return <h1>ExpenseView</h1>;
+  }
+  
